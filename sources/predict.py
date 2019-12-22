3a676ace30111e4a0b11e53b4bc1075ce46c6698
LinhNVC <conglinh97@gmail.com>
2019-12-22 21:42:10 +0700

    Adding all code

---
+++ sources/predict.py
@@ -0,0 +1,36 @@
+# restore our data structures
+import pickle
+data = pickle.load( open( "training_data", "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+# import intents file
+import json
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+model.load('./model.tflearn')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# bag of words
+def bow(sentence, words, show_details=False):
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+
+print(bow('I want to special food', words))
